e9dd136f0864f2ecac57e7bebfaaf626c470be2b
SeidAliyev <s.aliyev1214@gmail.com>
2024-04-04 16:27:26 +0400

    said

---
+++ 4aprel/task1S/index.js
@@ -0,0 +1,61 @@
+"use strict";
+
+let arr = [14, 20, 35, 40, 57, 60, 100];
+
+// function bolunen(array) {
+//     let cout=0
+//     for (let i = 0; i < array.length; i++) {
+//         if(array[i]%4==0 && array[i]%5==0){
+//             cout+=array[i]
+//         }
+//     }
+//     console.log(cout);
+// }
+
+// bolunen(arr)
+
+// let cumle = prompt('cumle daxil edin')
+// let herf = prompt('herfi daxil edin')
+
+// function herfler(ac,b) {
+//     let count=0
+//     for (let a = 0; a < ac.length; a++) {
+//         if (ac[a]==b) {
+//             count++
+//         }
+//     }
+//     console.log(count)
+// }
+
+// herfler(cumle,herf)
+
+// let numbers = [23,341,4000,9,99,817,0]
+
+// function reqemsayi(arr) {
+//     let count=0
+//     for (let i = 0; i < arr.length; i++) {
+//         for (let a = 0; a < String(arr[i]).length; a++) {
+//             count++
+//         }
+//         console.log(`${arr[i]} ededi ${count} reqemlidir`)
+
+//         count=0
+//     }
+// }
+
+// reqemsayi(numbers)
+
+let cumle = prompt("cumle daxil edin");
+
+function soz(ac) {
+  let b = ac.split(" ");
+  let c = b[0];
+  for (let i = 0; i < b.length; i++) {
+    if (b[i].length > c.length){
+        c=b[i]
+    }
+  }
+  console.log(c);
+}
+
+soz(cumle);

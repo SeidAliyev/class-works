7fc5aab37d88dd973484f597297de61fc352aba1
SeidAliyev <s.aliyev1214@gmail.com>
2024-04-05 18:55:33 +0400

    said

---
+++ 5aprel/task1/index.js
@@ -0,0 +1,20 @@
+console.log(Math.round(4.8));
+console.log(Math.ceil(4.6));
+console.log(Math.ceil(4.3));
+console.log(Math.floor(4.5));
+console.log(Math.max(4,5,6,7,8,9));
+
+
+let tarix = new Date()
+
+console.log(tarix.getFullYear());
+console.log(tarix.getMonth());
+console.log(tarix.getHours());
+console.log(tarix.getMinutes());
+console.log(tarix.getSeconds());
+console.log(tarix.getMilliseconds());
+console.log(tarix.getTime());
+// console.log(tarix.getDate());
+
+
+
